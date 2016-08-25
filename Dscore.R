cb8c4cc6028e3ef3908733b6521496f36cea79ab
changebio <changebio@yeah.net>
2016-08-25 09:01:25 +0800

    changed Dscore function calculated the Dscore of CAGE-Seq and GRO-Seq

--- Dscore.R
+++ Dscore.R
@@ -35,8 +35,8 @@ ggplot(gro.sg)+geom_line(data=gro.sg[gro.sg$gene=="+",],aes(x=Var1,y=value,colou
 
 ###cage from ENCODE
 cage.seq<- readRDS("/mnt/local-disk1/rsgeno2/huangyin/Rstudio/Iranman/data/cage_gr.rds")
-cage.sg.m<-lapply(cage.seq,function(x)ScoreMatrixBin(x,ucsc.gene.pt[strand(ucsc.gene.pt)=="+"],bin.num = 50,weight.col = "X"))
-cage.sg.p<-lapply(cage.seq,function(x)ScoreMatrixBin(x,ucsc.gene.pt[strand(ucsc.gene.pt)=="-"],bin.num = 50,weight.col = "X"))
+cage.sg.m<-lapply(cage.seq,function(x)ScoreMatrixBin(x,ucsc.gene.pt[strand(ucsc.gene.pt)=="+"],bin.num = 50,weight.col = "V5"))
+cage.sg.p<-lapply(cage.seq,function(x)ScoreMatrixBin(x,ucsc.gene.pt[strand(ucsc.gene.pt)=="-"],bin.num = 50,weight.col = "V5"))
 
 cage.sg<- melt(rbind(sapply(cage.sg.p,colMeans),sapply(cage.sg.m,colMeans)))
 cage.sg$gene<- c(rep("+",50),rep("-",50))
@@ -65,33 +65,35 @@ ggplot(gro.cage.sg)+geom_line(data=gro.cage.sg[gro.cage.sg$gene=="+",],aes(x=Var
 
 
 #Dscore function-----
-Dscore<- function(prom){
-  tss.sml<-ScoreMatrixList(gro.seq,prom,weight.col = "V5")
-  prom <- prom[as.numeric(Reduce(intersect,lapply(tss.sml,rownames)))]
-  tss.sml<- intersectScoreMatrixList(tss.sml)
-  tss.rs<- lapply(tss.sml,rowSums)
-  prom$wTAP <- (tss.rs[[2]]-tss.rs[[1]])/(tss.rs[[2]]+tss.rs[[1]])
-  prom$nTAP <- (tss.rs[[4]]-tss.rs[[3]])/(tss.rs[[4]]+tss.rs[[3]])
-  prom$cage <- (tss.rs[[6]]-tss.rs[[5]])/(tss.rs[[6]]+tss.rs[[5]])
-  prom$Cage <- NA
-  prom$Cage[tss.rs[[6]]+tss.rs[[5]]>=6] <- ((tss.rs[[6]]-tss.rs[[5]])/(tss.rs[[6]]+tss.rs[[5]]))[tss.rs[[6]]+tss.rs[[5]]>=6]
-  prom$TAP <- (tss.rs[[4]]-tss.rs[[2]]-tss.rs[[3]]+tss.rs[[1]])/(tss.rs[[4]]+tss.rs[[2]]-tss.rs[[3]]-tss.rs[[1]])
-  mcols(prom) <- cbind(mcols(prom),DataFrame(tss.rs))
-  return(prom)
+Dscore<- function(plus,minus,windows,wt = NULL){
+  plus<- ScoreMatrixBin(plus,windows,weight.col = wt,bin.num = 2 )
+  minus<- ScoreMatrixBin(minus,windows,weight.col = wt,bin.num = 2)
+  index<- intersect(rownames(plus),rownames(minus))
+  windows <- windows[as.numeric(index)]
+  plus<- rowSums(plus[index])*width(windows)
+  minus<- rowSums(minus[index])*width(windows)
+  Dscore<- (plus-minus)/(plus+minus)
+  mcols(windows)<- DataFrame(plus,minus,Dscore)
+  return(windows)
 }
 
 
 #Dscore distribution in region 500bp-----------
 ucsc.gene<- genes(txdb)
 ucsc.gene.pt<- promoters(ucsc.gene,upstream = 250,downstream = 250)
-gro.ds<- Dscore(ucsc.gene.pt)
+names(gro.seq)<- substring(names(gro.seq),first = 1,last = nchar(names(gro.seq))-9)
+gro.cage.seq<- c(gro.seq,cage.seq)
+gc.ds<- lapply(seq(from=1,to=length(gro.cage.seq),by=2),function(i,y)Dscore(y[[i+1]],y[[i]],ucsc.gene.pt,wt = "V5"),y=gro.cage.seq)
+names(gc.ds)<- substring(names(gro.cage.seq),first = 1,last = nchar(names(gro.cage.seq))-c(6,5))[seq(from=1,to=length(gro.cage.seq),by=2)]
+gc.ds.idx<- Reduce(intersect,lapply(gc.ds,names))
+gene.ds<- sapply(gc.ds,function(x)x[gc.ds.idx]$Dscore)
+gene.ds<- as.data.frame(gene.ds)
+gene.ds$strand<- as.character(strand(ucsc.gene.pt[gc.ds.idx]))
+saveRDS(gene.ds,file = "/mnt/local-disk1/rsgeno2/huangyin/Rstudio/Iranman/data/gene.ds.rds")
+gene.ds.gp<-melt(gene.ds)
 
-gene.ds <- as.data.frame(gro.ds)
-
-gene.ds.gp<-melt(gene.ds[,c(5,7:9)])
-ggplot(gene.ds.gp)+geom_histogram(data=gene.ds.gp[gene.ds.gp$strand=="+",],aes(x=value,y=..count..,fill=strand))+
-  geom_histogram(data=gene.ds.gp[gene.ds.gp$strand=="-",],aes(x=value,y=-..count..,fill=strand))+
-  facet_grid(. ~ variable)+
+ggplot(gene.ds.gp)+geom_histogram(aes(x=value,y=..count..,fill=strand))+
+  facet_wrap( ~ variable)+
   labs(x = "",y = " ",title="Histogram of D score of Promoters with 500bp width in K562") +
   theme(plot.title = element_text(color="black", size=20, face="bold.italic"),
         axis.title.x = element_text( face="bold",size=14),
@@ -104,6 +106,21 @@ ggplot(gene.ds.gp)+geom_histogram(data=gene.ds.gp[gene.ds.gp$strand=="+",],aes(x
   )
 
 
+ggplot(gene.ds.gp)+geom_histogram(data=gene.ds.gp[gene.ds.gp$strand=="+",],aes(x=value,y=..count..,fill=strand))+
+  geom_histogram(data=gene.ds.gp[gene.ds.gp$strand=="-",],aes(x=value,y=-..count..,fill=strand))+
+  facet_wrap( ~ variable)+
+  labs(x = "",y = " ",title="Histogram of D score of Promoters with 500bp width in K562") +
+  theme(plot.title = element_text(color="black", size=20, face="bold.italic"),
+        axis.title.x = element_text( face="bold",size=14),
+        axis.title.y = element_text(color="black", size=14, face="bold"),
+        legend.title =element_text(face = "bold", size = 14, color = "black"),
+        legend.text = element_text(face = "bold", size = 12),
+        axis.text.x = element_text(face="bold",size=14),
+        axis.text.y = element_text(face="bold", size=14),
+        strip.text.x = element_text(face = "bold",size=16)
+  )
+
+##
 
 grl.k4me3.ma.c <- lapply(k4me3.smt,function(x)resize(x,width = 240,fix="center"))
 grl.k4me3.ma.ds<- lapply(grl.k4me3.ma.c,Dscore)
